b4a4ab1d10fb6dcdd7195aab011f544a2024fab1
AkhmetZh <241371@astanait.edu.kz>
2025-02-05 20:02:54 +0500

    Add files via upload

---
+++ DLibraryApi/src/main/java/com/example/libraryapi/entities/Book.java
@@ -0,0 +1,72 @@
+
+package com.example.libraryapi.entities;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String genre;
+    private int publishedYear;
+
+    public Book() {
+    }
+
+    public Book(int id, String title, String author, String genre, int publishedYear) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.publishedYear = publishedYear;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getPublishedYear() {
+        return publishedYear;
+    }
+
+    public void setPublishedYear(int publishedYear) {
+        this.publishedYear = publishedYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                ", publishedYear=" + publishedYear +
+                '}';
+    }
+}
