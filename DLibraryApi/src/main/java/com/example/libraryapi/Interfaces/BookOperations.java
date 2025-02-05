8193f9bc03a6a2538c245c5d52f3bb438ee12c65
AkhmetZh <241371@astanait.edu.kz>
2025-02-05 20:50:53 +0500

    Update BookOperations.java

--- DLibraryApi/src/main/java/com/example/libraryapi/Interfaces/BookOperations.java
+++ DLibraryApi/src/main/java/com/example/libraryapi/Interfaces/BookOperations.java
@@ -3,15 +3,14 @@ package com.example.libraryapi.Interfaces;
 import com.example.libraryapi.entities.Book;
 import java.util.List;
 
-public interface BookOperations {
-
+public interface ReadOperations {
     List<Book> readAllBooks();
-
     Book readBookById(int id);
+}
 
+public interface WriteOperations {
     Book addBook(Book book);
-
     Book updateBook(Book book);
-
     boolean deleteBook(int id);
 }
+
